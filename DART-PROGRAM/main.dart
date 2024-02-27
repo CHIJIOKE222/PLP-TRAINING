89aea659be1245e0bbb5cd8b74ee2fba67de951c
CHIJIOKE222 <chijiokecharles93@gmail.com>
2024-02-27 12:12:41 +0100

    My second commit during plp training

---
+++ DART-PROGRAM/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name = "Femi";
+  int age = 28;
+  String nationality = "Nigeria";
+  bool ismarried = false;
+
+  print(name);
+  print(age);
+  print(nationality);
+print(ismarried);
+print("$name is a $age year old young professional who lives in $nationality and his currrent marriage status is $ismarried");
+  }
